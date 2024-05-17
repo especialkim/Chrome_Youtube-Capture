ab1fadcc5d9003ecc76d3ab3c9d12af9418da67a
YongMin Kim <especialkim@naver.com>
2024-05-17 23:55:50 +0900

    Added Title and Channel Name

--- content.js
+++ content.js
@@ -22,7 +22,7 @@ function createFloatingButtons() {
   floatingDiv.style.backgroundColor = 'white';
   floatingDiv.style.border = '1px solid black';
   floatingDiv.style.padding = '10px';
-  floatingDiv.style.zIndex = 10000;
+  floatingDiv.style.zIndex = '10000';
 
   // Capture 버튼 생성
   const captureBtn = document.createElement('button');
@@ -49,9 +49,45 @@ function createFloatingButtons() {
   document.body.appendChild(floatingDiv);
 }
 
+function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+  if (typeof radius === 'undefined') {
+    radius = 5;
+  }
+  if (typeof radius === 'number') {
+    radius = { tl: radius, tr: radius, br: radius, bl: radius };
+  } else {
+    const defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 };
+    for (let side in defaultRadius) {
+      radius[side] = radius[side] || defaultRadius[side];
+    }
+  }
+  
+  ctx.beginPath();
+  ctx.moveTo(x + radius.tl, y);
+  ctx.lineTo(x + width - radius.tr, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+  ctx.lineTo(x + width, y + height - radius.br);
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+  ctx.lineTo(x + radius.bl, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+  ctx.lineTo(x, y + radius.tl);
+  ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+  ctx.closePath();
+  
+  if (fill) {
+    ctx.fill();
+  }
+  if (stroke) {
+    ctx.stroke();
+  }
+}
+
 function captureScreenshot() {
   const videoElement = document.querySelector('video.html5-main-video');
-  if (videoElement) {
+  const titleElement = document.querySelector('h1.ytd-watch-metadata');
+  const channelElement = document.querySelector('ytd-channel-name a');
+
+  if (videoElement && titleElement && channelElement) {
     // 탭에 포커스 주기
     window.focus();
     
@@ -68,18 +104,48 @@ function captureScreenshot() {
     const seconds = Math.floor(currentTime % 60).toString().padStart(2, '0');
     const timestamp = `📌 ${hours}:${minutes}:${seconds}`;
 
+    // 채널명과 제목 가져오기
+    let title = titleElement.innerText;
+    const channelName = `@ ${channelElement.innerText}`;
+
+    // 제목이 50글자보다 길면 뒤를 ...으로 표시
+    if (title.length > 50) {
+      title = title.substring(0, 50) + '...';
+    }
+
     // 타임스탬프 추가
-    ctx.font = '60px Arial';
+    ctx.font = '50px Arial';
     ctx.textBaseline = 'bottom';
-    const textWidth = ctx.measureText(timestamp).width;
+    let textWidth = ctx.measureText(timestamp).width;
 
-    // 글자 영역에만 배경색 추가
+    // 타임스탬프 배경 그리기
+    const timestampY = canvas.height - 26;
     ctx.fillStyle = 'rgba(0, 0, 0, 0.5)'; // 반투명 검은색 배경
-    ctx.fillRect(canvas.width - textWidth - 50, canvas.height - 100, textWidth + 20, 70);
+    roundRect(ctx, canvas.width - textWidth - 50, timestampY - 64, textWidth + 20, 70, 10, true);
 
-    // 글자 그리기
+    // 타임스탬프 글자 그리기
     ctx.fillStyle = 'white';
-    ctx.fillText(timestamp, canvas.width - textWidth - 40, canvas.height - 30);
+    ctx.fillText(timestamp, canvas.width - textWidth - 40, timestampY);
+
+    // 채널명과 제목 추가
+    ctx.font = '40px Arial';
+    ctx.textBaseline = 'bottom';
+    const titleWidth = ctx.measureText(title).width;
+    const channelWidth = ctx.measureText(channelName).width;
+    const maxWidth = Math.max(titleWidth, channelWidth);
+
+    // 배경 그리기
+    const textY = canvas.height - 60; // 오른쪽 타임스탬프와 같은 y 기준으로 설정
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.5)'; // 반투명 검은색 배경
+    roundRect(ctx, 20, textY - 70, maxWidth + 40, 110, 10, true); // 둥근 모서리 적용
+
+    // 채널명 그리기
+    ctx.fillStyle = 'white';
+    ctx.fillText(channelName, 40, textY - 20);
+
+    // 제목 그리기
+    ctx.fillStyle = 'white';
+    ctx.fillText(title, 40, textY + 25);
 
     canvas.toBlob(function(blob) {
       navigator.clipboard.write([new ClipboardItem({ 'image/png': blob })]).then(function() {
@@ -90,25 +156,24 @@ function captureScreenshot() {
       });
     }, 'image/png');
   } else {
-    console.log('Video element not found.');
+    console.log('Video element or title or channel not found.');
   }
 }
-
 
 function showNotification(message) {
   const notification = document.createElement('div');
   notification.innerText = message;
   notification.style.position = 'fixed';
-  notification.style.bottom = '10px';
-  notification.style.right = '10px';
+  notification.style.bottom = '50%';
+  notification.style.right = '50%';
   notification.style.backgroundColor = 'rgba(0,0,0,0.7)';
   notification.style.color = 'white';
   notification.style.padding = '10px';
   notification.style.borderRadius = '5px';
-  notification.style.zIndex = 10000;
+  notification.style.zIndex = '10000';
   document.body.appendChild(notification);
   
   setTimeout(() => {
     notification.remove();
-  }, 3000); // 3초 후에 알림을 제거
+  }, 1500); // 1.5초 후에 알림을 제거
 }
--- popup.js
+++ popup.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('onBtn').addEventListener('click', function() {
     console.log('On button clicked');
-    
+
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { action: 'showFloatingButtons' }, function(response) {
         if (chrome.runtime.lastError) {
           console.error('Error sending message:', chrome.runtime.lastError.message);
         } else {
           console.log('Response from content script:', response);
+          window.close(); // 팝업 닫기
         }
       });
     });
